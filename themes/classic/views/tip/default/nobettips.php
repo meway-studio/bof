2865adbea1fcead23449984b22f7ef251319d0b6
wmsamolet <wmsamolet@gmail.com>
2014-06-24 20:06:54 +0400

    Добавлена кнопка in-running tips на главной страницеё

--- themes/classic/views/tip/default/nobettips.php
+++ themes/classic/views/tip/default/nobettips.php
@@ -1,24 +1,33 @@
+<?php $inRunningTip = Yii::app()->request->getQuery( 'in_running', false ); ?>
 <div class="site-width">
-	<div class="active-tips">
-		
-		<div class="title">
-			<span class="bold"><?php echo Yii::t('tips', 'Без ставок') ?></span>
-			<span> <?php echo Yii::t('tips', 'Советов') ;?></span>
-		</div>
-		
-		<?php $this->widget('zii.widgets.CListView', array(
-			'dataProvider' => $dataProvider,
-			'itemView'     => '_nb_tip',
-			'template'=>'{pager}<br />{items} {pager}',
-			'afterAjaxUpdate' => 'function(id,data){scroll(0,150);}',
-			'pager' => array(
-				'header'=>'<span></span>',
-				'prevPageLabel'=>'&larr;',
-				'firstPageLabel'=>'&larr;',
-				'nextPageLabel'=>'&rarr;',
-				'lastPageLabel'=>'&rarr;',
-			),
-		)); ?>
-		
-	</div>
+    <div class="active-tips">
+
+        <div class="title">
+            <?php if ($inRunningTip): ?>
+                <span class="bold"><?php echo Yii::t( 'tips', 'Советы по' ); ?></span>
+                <span> <?php echo Yii::t( 'tips', 'по ходу игры' ); ?></span>
+            <?php else: ?>
+                <span class="bold"><?php echo Yii::t( 'tips', 'Без ставок' ) ?></span>
+                <span> <?php echo Yii::t( 'tips', 'Советов' ); ?></span>
+            <?php endif ?>
+        </div>
+
+        <?php $this->widget(
+            'zii.widgets.CListView',
+            array(
+                'dataProvider'    => $dataProvider,
+                'itemView'        => '_nb_tip',
+                'template'        => '{pager}<br />{items} {pager}',
+                'afterAjaxUpdate' => 'function(id,data){scroll(0,150);}',
+                'pager'           => array(
+                    'header'         => '<span></span>',
+                    'prevPageLabel'  => '&larr;',
+                    'firstPageLabel' => '&larr;',
+                    'nextPageLabel'  => '&rarr;',
+                    'lastPageLabel'  => '&rarr;',
+                ),
+            )
+        ); ?>
+
+    </div>
 </div>
