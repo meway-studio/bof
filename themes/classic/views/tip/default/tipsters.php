a03b4d42ce0bfc95bf981c1b9df69c000c8de9fe
wmsamolet <wmsamolet@gmail.com>
2014-07-24 15:42:21 +0400

    [closed#7] Cтраница /stats-all-time

--- themes/classic/views/tip/default/tipsters.php
+++ themes/classic/views/tip/default/tipsters.php
@@ -112,64 +112,69 @@
 
     <hr>
 
-    <div class="stats-all-time">
-        <div class="title">
-            <span class="bold"><?php echo Yii::t( 'themes', 'Аналитики, покинувшие проект' ); ?></span>
-        </div>
-        <div class="profile">
-            <?php foreach ($model2 AS $data): ?>
-                <div class="tipster-inf">
-                    <div class="top">
-                        <div style="overflow: hidden;">
-                            <div style="display: inline-block; position: relative;">
-                                <?php echo CHtml::image( $data->photoOriginal, '', array( 'class' => 'image' ) ); ?>
-                                <img class="img_pattern" src="<?php echo Yii::app(
-                                )->theme->baseUrl; ?>/css/images/tips/img_pattern_fff.png">
+    <?php if (count( $model2 )): ?>
+        <div class="stats-all-time">
+            <div class="title">
+                <span class="bold"><?php echo Yii::t( 'themes', 'Аналитики, покинувшие проект' ); ?></span>
+            </div>
+            <div class="profile">
+                <?php foreach ($model2 AS $data): ?>
+                    <div class="tipster-inf">
+                        <div class="top">
+                            <div style="overflow: hidden;">
+                                <div style="display: inline-block; position: relative;">
+                                    <?php echo CHtml::image( $data->photoOriginal, '', array( 'class' => 'image' ) ); ?>
+                                    <img class="img_pattern" src="<?php echo Yii::app(
+                                    )->theme->baseUrl; ?>/css/images/tips/img_pattern_fff.png">
+                                </div>
+                                <div class="some-inf">
+                                    <span><?php echo Yii::t( 'themes', 'Общая прибыль' ); ?></span>
+                                    <span class="big"><?php echo $data->tipster->profit; ?></span>
+                                    <span><?php echo Yii::t( 'themes', 'Прибыль' ); ?></span>
+                                    <span><?php echo Yii::t( 'themes', 'ROI (Доход)' ); ?>
+                                        <span> <?php echo $data->tipster->yield; ?>%</span>
+                                    </span>
+                                </div>
                             </div>
-                            <div class="some-inf">
-                                <span><?php echo Yii::t( 'themes', 'Общая прибыль' ); ?></span>
-                                <span class="big"><?php echo $data->tipster->profit; ?></span>
-                                <span><?php echo Yii::t( 'themes', 'Прибыль' ); ?></span>
-                                <span><?php echo Yii::t( 'themes', 'ROI (Доход)' ); ?>
-                                    <span> <?php echo $data->tipster->yield; ?>%</span>
-                                </span>
+                            <span class="name"><?php echo $data->FullName; ?></span>
+                            <span class="status"><?php echo $data->tipster->rank; ?></span>
+                            <span class="devis"><?php echo $data->tipster->comment; ?></span>
+                            <div class="some-inf-bottom">
+                                <a class="stats" href="<?php echo Yii::app()->createUrl(
+                                    '/tip/default/stat',
+                                    array( 'id' => $data->id )
+                                ); ?>">
+                                    <span class="pixel active"></span>
+                                    <span><?php echo Yii::t( 'tips', 'Статистика' ); ?></span>
+                                </a> <a class="tips" href="<?php echo Yii::app()->createUrl(
+                                    '/tip/default/list',
+                                    array( 'tipster' => $data->id )
+                                ); ?>">
+                                    <span class="pixel active"></span>
+                                    <span class="how-mutch"><?php echo $data->tipster->activeCount; ?></span>
+                                    <span><?php echo Yii::t( 'themes', 'Советы' ); ?></span>
+                                </a> <a class="subscribe" href="<?php echo Yii::app()->createUrl(
+                                    '/tip/default/subscription'
+                                ); ?>" style="margin-right: 0px;">
+                                    <span class="pixel active"></span>
+                                    <span><?php echo Yii::t( 'themes', 'Подписки' ); ?></span>
+                                </a>
                             </div>
                         </div>
-                        <span class="name"><?php echo $data->FullName; ?></span>
-                        <span class="status"><?php echo $data->tipster->rank; ?></span>
-                        <span class="devis"><?php echo $data->tipster->comment; ?></span>
-                        <div class="some-inf-bottom">
-                            <a class="stats" href="<?php echo Yii::app()->createUrl( '/tip/default/stat', array( 'id' => $data->id ) ); ?>">
-                                <span class="pixel active"></span>
-                                <span><?php echo Yii::t( 'tips', 'Статистика' ); ?></span>
-                            </a> <a class="tips" href="<?php echo Yii::app()->createUrl(
-                                '/tip/default/list',
-                                array( 'tipster' => $data->id )
-                            ); ?>">
-                                <span class="pixel active"></span>
-                                <span class="how-mutch"><?php echo $data->tipster->activeCount; ?></span>
-                                <span><?php echo Yii::t( 'themes', 'Советы' ); ?></span>
-                            </a> <a class="subscribe" href="<?php echo Yii::app()->createUrl(
-                                '/tip/default/subscription'
-                            ); ?>" style="margin-right: 0px;">
-                                <span class="pixel active"></span>
-                                <span><?php echo Yii::t( 'themes', 'Подписки' ); ?></span>
-                            </a>
+                        <div class="bottom">
+                            <span><?php echo $data->tipster->tips; ?>
+                                <span><?php echo Yii::t( 'themes', 'Количество советов' ); ?></span>
+                            </span>
+                            <span><?php echo $data->tipster->winrate; ?>
+                                <span><?php echo Yii::t( 'themes', 'Количество побед' ); ?></span>
+                            </span>
+                            <span style="margin-right: 0;"><?php echo $data->tipster->odds; ?>
+                                <span><?php echo Yii::t( 'themes', 'Средний коэффициент' ); ?></span>
+                            </span>
                         </div>
                     </div>
-                    <div class="bottom">
-                        <span><?php echo $data->tipster->tips; ?>
-                            <span><?php echo Yii::t( 'themes', 'Количество советов' ); ?></span>
-                        </span>
-                        <span><?php echo $data->tipster->winrate; ?>
-                            <span><?php echo Yii::t( 'themes', 'Количество побед' ); ?></span>
-                        </span>
-                        <span style="margin-right: 0;"><?php echo $data->tipster->odds; ?>
-                            <span><?php echo Yii::t( 'themes', 'Средний коэффициент' ); ?></span>
-                        </span>
-                    </div>
-                </div>
-            <?php endforeach; ?>
+                <?php endforeach; ?>
+            </div>
         </div>
-    </div>
+    <?php endif ?>
 </div>
