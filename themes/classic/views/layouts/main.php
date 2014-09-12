ca1f055b40e7ab811371dc4cf904e57049385445
wmsamolet <wmsamolet@gmail.com>
2014-09-12 16:02:19 +0400

    Фиксы

--- themes/classic/views/layouts/main.php
+++ themes/classic/views/layouts/main.php
@@ -15,7 +15,7 @@ Yii::app()->clientScript->registerScriptFile( Yii::app()->theme->baseUrl . '/js/
 Yii::app()->clientScript->registerCssFile( Yii::app()->theme->baseUrl . '/css/jquery.toastmessage.css' );
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo Yii::app()->language ?>" lang="<?php echo Yii::app()->language ?>" class="lang-<?php echo Yii::app()->language ?>">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <meta name="language" content="en"/>
--- themes/classic/views/tip/default/list.php
+++ themes/classic/views/tip/default/list.php
@@ -1,29 +1,45 @@
 <div class="site-width">
-	<div class="active-tips">
-		
-		<div class="title">
-			<span>
-				<?php if($active==null):?>
-					<?php echo $user!=null ? $user->FullName : Yii::t('tips', 'Все советы на весь период');?>
-				<?php else:?>
-					<?php echo $active==1 ? Yii::t('tips', 'Активные Советы') : Yii::t('tips', 'Все Советы') ;?>
-				<?php endif;?>
-			</span>
-		</div>
-		
-		<?php $this->widget('zii.widgets.CListView', array(
-			'dataProvider' => $dataProvider,
-			'itemView'     => '_tip',
-			'template'=>'{pager}<br />{items} {pager}',
-			'afterAjaxUpdate' => 'function(id,data){scroll(0,150);}',
-			'pager' => array(
-				'header'=>'<span></span>',
-				'prevPageLabel'=>'&larr;',
-				'firstPageLabel'=>'&larr;',
-				'nextPageLabel'=>'&rarr;',
-				'lastPageLabel'=>'&rarr;',
-			),
-		)); ?>
-		
-	</div>
+    <div class="active-tips">
+
+        <div class="title">
+            <span>
+                <?php if ($active == null): ?>
+                    <?php echo $user != null ? Yii::t(
+                        'tips',
+                        'Все советы от <b>{FullName}</b> за весь период',
+                        array( '{FullName}' => $user->FullName )
+                    ) : Yii::t( 'tips', 'Все советы на весь период' ); ?>
+                <?php else: ?>
+                    <?php if ($active == 1): ?>
+                        <?php echo
+                        $user != null ? Yii::t( 'tips', 'Активные советы от <b>{FullName}</b>' ) : Yii::t( 'tips', 'Активные Советы' ) ?>
+                    <?php else: ?>
+                        <?php echo $user != null ? Yii::t(
+                            'tips',
+                            'Все советы от <b>{FullName}</b>',
+                            array( '{FullName}' => $user->FullName )
+                        ) : Yii::t( 'tips', 'Все советы' ); ?>
+                    <?php endif ?>
+                <?php endif; ?>
+            </span>
+        </div>
+
+        <?php $this->widget(
+            'zii.widgets.CListView',
+            array(
+                'dataProvider'    => $dataProvider,
+                'itemView'        => '_tip',
+                'template'        => '{pager}<br />{items} {pager}',
+                'afterAjaxUpdate' => 'function(id,data){scroll(0,150);}',
+                'pager'           => array(
+                    'header'         => '<span></span>',
+                    'prevPageLabel'  => '&larr;',
+                    'firstPageLabel' => '&larr;',
+                    'nextPageLabel'  => '&rarr;',
+                    'lastPageLabel'  => '&rarr;',
+                ),
+            )
+        ); ?>
+
+    </div>
 </div>
